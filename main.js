374459350a3e57dfda834a0ba2c34913212acbf3
Akshat-260604 <111489980+Akshat-260604@users.noreply.github.com>
2024-02-05 21:35:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Frontend Developer",
+    "Python Developer",
+    "Researcher",
+    "AIML student",
+    "Academic Tutor",
+    "Content Writer",
+  ],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
